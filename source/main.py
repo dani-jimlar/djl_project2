a393aedb7c811137a03b3e979dad85eaefb9e073
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-09-10 03:43:51 +0000

    changes in main

--- source/main.py
+++ source/main.py
@@ -1,11 +1,25 @@
 """
-Main cli or app entry point
+This fun prints descriptive analytics of a data set
 """
 
-def multiply(x,y):
-    return(x*y)
+import pandas as pd
+ 
+def print_ds_info(data):
+    """
+    parameter:data
+    uses data.columns and describe() to print descriptive info of dataset
+    """
+    print("The variables in this data set are the following:",list(data.columns))
+    print("Data behaves as follows:\n", data.describe())
 
 
 if __name__ == "__main__":
-    out=multiply(x*y)
-    print("the result of fun multiply is",{out})
+
+    # Read the CSV file
+    my_data = pd.read_csv("/", encoding="ISO-8859-1")
+
+    # Print dataset info
+    print_ds_info(my_data)
+
+
+
