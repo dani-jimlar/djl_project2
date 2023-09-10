93285784a522b289819598a71161e0d8db0f5356
dani-jimlar <143829673+dani-jimlar@users.noreply.github.com>
2023-09-10 05:31:50 +0000

    changes in main

--- source/main.py
+++ source/main.py
@@ -3,6 +3,8 @@ This fun prints descriptive analytics of a data set
 """
 
 import pandas as pd
+import seaborn
+import matplotlib.pyplot as plt
  
 def print_ds_info(data):
     """
@@ -11,7 +13,17 @@ def print_ds_info(data):
     """
     print("The variables in this data set are the following:",list(data.columns))
     print("Data behaves as follows:\n", data.describe())
-
+ 
+def data_vis(data):
+    """this fun generates a simple plot of the data set"""
+    sns.barplot(
+    data=data,  x="descrip",y="sexostt",errorbar=None)
+    plt.xlabel("Age group")
+    plt.ylabel("Number of inmmates")
+    plt.title("Number of inmates by age group in Mexican prisons in 2021")
+    locs, labels = plt.xticks()
+    plt.setp(labels, rotation=90)
+    plt.savefig('plot.png', dpi=300, bbox_inches='tight')
 
 if __name__ == "__main__":
 
@@ -20,6 +32,8 @@ if __name__ == "__main__":
 
     # Print dataset info
     print_ds_info(my_data)
+    # generate graph
+    
 
 
 
